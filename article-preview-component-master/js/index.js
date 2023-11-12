e04747342b81266858d4548dd71a21d92cd4b345
Xasanov Ibrohim <you@example.com>
2023-11-12 22:44:24 +0500

    deleted artice preview component

--- article-preview-component-master/js/index.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('.info__wrapper-button');
-const infoSocials = document.querySelector(".info__socials")
-button.addEventListener("click", () => {
-  infoSocials.classList.toggle("active")
-})
